1e083e77d037839948a90b43bc99df4b8483d52d
mohammedafeef <mohammedafeef.maf@gmail.com>
2026-02-16 22:09:41 +0530

    [feat]: rewrite the all the page file

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/:path((?!^$|\\/$))*',
+  //       destination: '/',
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
+  async rewrites() {
+    return [
+      {
+        source: '/:path((?!^$).*)',
+        destination: '/',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
